d61948fcc1c62ddd6ce45df7c6b2d1a0a24d5908
Monica Delgado <mnca.delgado@gmail.com>
2022-03-07 10:52:20 -1100

    using constructors

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,20 +1,34 @@
+class Product {
+    tittle = 'DEFAULT';
+    imageUrl;
+    description;
+    price;
+
+    constructor(title, image, desc, price) {
+        this.tittle = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.pice = price;
+    }
+}
+
 const productList = {
     products: [
-        {
-            title: 'A Pillow',
-            imageUrl:
-               'https://ctl.s6img.com/society6/img/0GugJ9_kCZJpxR9UZXeb29s_mkc/w_1500/pillows/~artwork,fw_3502,fh_3502,fx_-219,fy_-612,iw_3942,ih_5256/s6-original-art-uploads/society6/uploads/misc/60d6525f83c740b983345b90187d3db1/~~/arch-balance-green-pillows.jpg',
-            price: 19.99,
-            description: 'decorative pillow'
-        
-        },
-        {
-            tittle: 'A Carpet',
-            imageUrl:
-               'https://iranguidance.com/wp-content/uploads/2020/01/BottehJogheh-iranguidance-248x300.jpg',
-            price: 89.99,
-            description: 'A carpet which you might like - or not.'
-        }
+        new Product(
+            'A Pillow',
+            'https://ctl.s6img.com/society6/img/0GugJ9_kCZJpxR9UZXeb29s_mkc/w_1500/pillows/~artwork,fw_3502,fh_3502,fx_-219,fy_-612,iw_3942,ih_5256/s6-original-art-uploads/society6/uploads/misc/60d6525f83c740b983345b90187d3db1/~~/arch-balance-green-pillows.jpg',
+            'decorative pillow',
+            19.99,
+
+
+        ),
+        new Product(
+            'A Carpet',
+            'https://iranguidance.com/wp-content/uploads/2020/01/BottehJogheh-iranguidance-248x300.jpg',
+            'A carpet which you might like - or not.',
+            89.99
+
+        )
     ],
     render() {
         const renderHook = document.getElementById('app');
