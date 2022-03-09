82e7419ae75da97b1a1913d0d52fb87ecdf5169c
Monica Delgado <mnca.delgado@gmail.com>
2022-03-09 08:45:58 -1100

    connected multiple classes

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,52 +1,70 @@
+//This class put data together
 class Product {
     constructor(title, image, desc, price) {
-        this.tittle = title;
+        this.title = title;
         this.imageUrl = image;
         this.description = desc;
-        this.pice = price;
+        this.price = price;
     }
 }
 
-const productList = {
-    products: [
+//This class holds the logic of how a product looks like in the shop
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+           <div>
+              <img src="${this.product.imageUrl}" alt="${this.product.title}">
+              <div class="product-item__content">
+                 <h2>${this.product.title}</h2>
+                 <h3>\$${this.product.price}</h3>
+                 <p>${this.product.description}</p>
+                 <button>Add to Cart</button>
+              </div>
+           </div>
+        `;
+        return prodEl;
+    }
+}
+
+
+class ProducList {
+    products = [
         new Product(
-            'A Pillow',
+            'Pillow',
             'https://ctl.s6img.com/society6/img/0GugJ9_kCZJpxR9UZXeb29s_mkc/w_1500/pillows/~artwork,fw_3502,fh_3502,fx_-219,fy_-612,iw_3942,ih_5256/s6-original-art-uploads/society6/uploads/misc/60d6525f83c740b983345b90187d3db1/~~/arch-balance-green-pillows.jpg',
-            'decorative pillow',
-            19.99,
-
-
+            'Decorative pillow',
+            19.99
         ),
         new Product(
-            'A Carpet',
+            'Carpet',
             'https://iranguidance.com/wp-content/uploads/2020/01/BottehJogheh-iranguidance-248x300.jpg',
             'A carpet which you might like - or not.',
             89.99
-
         )
-    ],
+    ];
+
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-               <div>
-                  <img src="${prod.imageUrl}" alt="${prod.title}">
-                  <div class="product-item__content">
-                     <h2>${prod.title}</h2>
-                     <h3>$/${prod.price}</h3>
-                     <p>${prod.description}</p>
-                     <button>Add to Cart</button>
-                  </div>
-               </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
+            
         }
         renderHook.append(prodList);
     }
-};
+}
 
+const productList = new ProducList();
 productList.render();
+
